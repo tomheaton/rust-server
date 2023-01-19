9eb0af2ccc07f46d0cae8992e9d79e3e9d5258ca
Tom Heaton <thomas.m.heaton@gmail.com>
2023-01-19 23:05:13 +0000

    rocket :rocket:

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("rust-server");
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    return "tomheaton";
+}
+
+#[get("/<name>")]
+fn hello(name: &str) -> String {
+    return format!("Hello, {}!", name);
+}
+
+#[rocket::main]
+async fn main() -> Result<(), rocket::Error> {
+    let _rocket = rocket::build()
+        .mount("/", routes![index])
+        .mount("/hello", routes![hello])
+        .launch()
+        .await?;
+
+    return Ok(());
 }
