ddb1a671daf562ca7f0bb68a7993bc4dd90e217b
Tom Heaton <thomas.m.heaton@gmail.com>
2023-01-19 23:18:25 +0000

    json work

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,50 @@
 #[macro_use]
 extern crate rocket;
 
+use rocket::serde::json::Json;
+
+struct Response<T> {
+    success: bool,
+    message: String,
+    data: T,
+}
+
+struct User {
+    id: i32,
+    username: String,
+}
+
 #[get("/")]
 fn index() -> &'static str {
     return "tomheaton";
 }
 
+/*#[get("/user", format = "json")]
+fn user() -> User {
+    return User {
+        id: 1,
+        username: "tomheaton".to_string(),
+    };
+}*/
+
+#[get("/")]
+fn world() -> &'static str {
+    return "Hello, World!";
+}
+
 #[get("/<name>")]
-fn hello(name: &str) -> String {
+// fn hello(name: &str) -> String {
+fn hello(name: &str) -> Json<String> {
     return format!("Hello, {}!", name);
 }
+
 
 #[rocket::main]
 async fn main() -> Result<(), rocket::Error> {
     let _rocket = rocket::build()
+        // .mount("/", routes![index, user])
         .mount("/", routes![index])
-        .mount("/hello", routes![hello])
+        .mount("/hello", routes![hello, world])
         .launch()
         .await?;
 
